4bc94e7fc427784087e865396fe10b60b6752378
Satvika Eda <satvika164.reddy@gmail.com>
2020-03-02 21:36:18 +0530

    Add files via upload

---
+++ HouseConstructionCost.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class HouseCost {
+
+public double estimateCost(String material, double area, boolean fullyAutomated) {
+	// TODO Auto-generated method stub
+	if(material.equals("standardMaterial") && fullyAutomated == false)
+	{
+	return 1200*area;
+	}
+	else if(material.equals("aboveStandardMaterial") && fullyAutomated == false)
+	{
+	return 1500*area;
+	}
+	else if(material.equals("highStandardMaterial") && fullyAutomated == false)
+	{
+	return 1800*area;
+	}
+	else
+	{
+	return 2500*area;
+	}
+}
+}
